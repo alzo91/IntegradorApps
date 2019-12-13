d1d6f435264a2db9675515208a99b57977985030
Alisson Zotarelli <alzo.zotarelli@gmail.com>
2019-12-13 00:03:50 -0300

    inclusão de headers

--- src/app.js
+++ src/app.js
@@ -19,6 +19,14 @@ class App {
     this.server.use(cors({ origin: false, optionsSuccessStatus: 200 }));
     this.server.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
+      //
+      // res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      );
+      //
       res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
